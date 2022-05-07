425d9777723144808975f928398769c12e90a5ab
goldimir <56108227+goldimir@users.noreply.github.com>
2022-05-07 20:47:52 +0800

    Update STAT5102C4.py

--- STAT5102C4.py
+++ STAT5102C4.py
@@ -8,9 +8,7 @@ Created on Thu Feb 17 09:06:37 2022
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 from matplotlib import cm
-import yfinance as yf  
 import pandas as pd
-import quandl
 import numpy as np
 import scipy.stats
 import statsmodels.api as sm
